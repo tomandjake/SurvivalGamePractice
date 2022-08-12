0054a3f1c103b7173b267182f6c72407d1f7971d
wang chuhan <stardustwang@lilith.com>
2022-08-12 10:56:06 +0800

    init Config, Source, .uproject

---
+++ SurvivalGame/Source/SurvivalGame/SurvivalGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivalGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivalGame, "SurvivalGame" );
---
+++ SurvivalGame/Source/SurvivalGame/SurvivalGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivalGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVALGAME_API ASurvivalGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
